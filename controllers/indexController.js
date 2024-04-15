f6862b14d4688b9792e1462b942ece3c0e4efc25
JoaquinKusinsky <jkusinsky@udesa.edu.ar>
2024-04-15 13:48:13 -0300

    desarrollo search results

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,6 +3,12 @@ const datos = require('../db/datos')
 const indexController= {
     vistaDeindex: function (req, res){
         return res.render("index", {productos: datos.productos, usuario : datos.usuario})
-    }}
+    },
+
+    vistaDeSearch: function (req, res){
+        return res.render("search-results", {productos: datos.productos, usuario : datos.usuario})
+    },
+
+}
 
 module.exports= indexController;
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,6 @@ var router = express.Router();
 const indexController= require("../controllers/indexController")
 
 router.get('/', indexController.vistaDeindex )
+router.get('/search', indexController.vistaDeSearch )
 
 module.exports = router;
