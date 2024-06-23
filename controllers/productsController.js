9c0ba678cfca5c88bf7d26975e8dd9c2effc24cd
milenasorin <msorin@udesa.edu.ar>
2024-06-23 16:22:14 -0300

    Consigna punto 8 - Crear productos: desarrollo completo + validaciones y control de acceso.

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,13 +1,56 @@
 const db = require("../database/models")
 const op = db.Sequelize.Op;
+const {validationResult } = require('express-validator')
 
 const productsController = {
     vistaDeProducto: function (req, res) {
         return res.render("product", { productos: datos.productos, usuario: datos.usuario })
     },
+
     vistaAdd: function (req, res) {
-        return res.render("product-add", { productos: datos.productos, usuario: datos.usuario })
+        if (req.session.user == undefined) {
+            return res.redirect("/users/login");
+        }
+        return res.render("product-add")
+    },
+
+    storeProduct: function (req, res) {
+
+        let errors = validationResult(req)
+      
+         if (errors.isEmpty()) {
+        let forms = req.body;
+
+
+        let product = {
+            include: [
+                { association: "usuario" }
+            ],
+            nombreProducto: forms.Nombre,
+            imagenProducto: forms.Imagen,
+            descripcionProducto: forms.Descripcion,
+            idUsuario: req.session.user.id,
+
+        }
+
+        db.Producto.create(product)
+            .then((result) => {
+                //return res.send(result)
+                return res.redirect("/");
+            }).catch((err) => {
+                return console.log(err);
+            });
+
+        }
+        else {
+            return res.render('product-add', {
+                errors: errors.mapped(),
+            old: req.body
+        })
     }
+
+
+    },
 
 
 }
--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const router = express.Router();
-
 const productsController = require('../controllers/productsController');
+const { body } = require('express-validator');
+
+let validationsAdd =[
+    body('Imagen')
+     .notEmpty().withMessage('Debes ingresar una imagen del producto').bail(),
+    body('Nombre')
+     .notEmpty().withMessage('Debes ingresar el nombre del producto ').bail(),
+    body('Descripcion')
+     .notEmpty().withMessage('Debes ingresar la descripcion del producto ').bail()
+    
+    ]
+
+
 router.get("/", productsController.vistaDeProducto)
+router.post("/Add", validationsAdd, productsController.storeProduct)
 router.get("/Add", productsController.vistaAdd)
+
 module.exports = router;
