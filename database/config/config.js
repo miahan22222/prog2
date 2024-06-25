05c5ba1dd9286f27ab9ebedf0269bcf69554a7e7
miahan22222 <mimihanono@gmail.com>
2024-06-24 23:28:46 -0300

    ultimos arreglos

--- database/config/config.js
+++ database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": "root",
-    "database": "data2",
+    "database": "data2", //nombre de SQL del schema
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
